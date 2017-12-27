85250a0b32ba82aa024188273f0d8863c2ce1161
pamperboy <marc_smalbrugge@live.nl>
2017-12-27 14:31:06 +0100

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,10 @@
-artist_1 = Artist.new(name: "herp")
+Artist.destroy_all
+
+artist_1 = Artist.create(name: "Slipknot")
+artist_2 = Artist.create(name: "Beatles")
+artist_3 = Artist.create(name: "ACDC")
+artist_4 = Artist.create(name: "John Butler Trio")
+artist_5 = Artist.create(name: "Deep Purple")
+artist_6 = Artist.create(name: "Red Hot Chilly Peppers")
+
+song_1 = Song.create(name: "Better Than", artist_id: artist_4)
