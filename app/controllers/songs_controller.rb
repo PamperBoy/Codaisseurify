971426194a26e79a7a09cc7e2145b6c1bd784b31
pamperboy <marc_smalbrugge@live.nl>
2017-12-27 20:59:08 +0100

    Add song add functionality + update view + nested routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,32 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+
+
+  def new
+    @song = @artist.songs.new
+  end
+
+  def create
+    @song = @artist.songs.new(song_params)
+
+    if @song.save
+      redirect_to @artist, notice: "Song created"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:artist_id])
+  end
+
+  def song_params
+    params
+      .require(:song)
+      .permit(
+        :name
+      )
+  end
 end
