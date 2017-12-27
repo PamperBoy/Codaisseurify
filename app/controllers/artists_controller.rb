5e8ceae416f7fda4eb4a9c3abf35497db7446ff7
pamperboy <marc_smalbrugge@live.nl>
2017-12-27 21:11:59 +0100

    Add Artist add functionality

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,8 +7,32 @@ class ArtistsController < ApplicationController
 
   def show; end
 
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      redirect_to @artist, notice: "Artist created"
+    else
+      render :new
+    end
+  end
+
+  private
+
   def set_artist
     @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params
+      .require(:artist)
+      .permit(
+        :name
+      )
   end
 
 end
