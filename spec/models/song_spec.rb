fb1a1e9554dfc6b96eb4a213b8334a72a0ded0fa
pamperboy <marc_smalbrugge@live.nl>
2017-12-28 22:58:16 +0100

    Add model relation tests

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,9 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  it "is invalid without a song name" do
-    artist = Artist.new(name: "")
-      artist.valid?
-      expect(artist.errors).to have_key(:name)
+  describe "Validation" do
+
+    it "is invalid without a song name" do
+      artist = Artist.new(name: "")
+        artist.valid?
+        expect(artist.errors).to have_key(:name)
+    end
+  end
+
+  describe "Belongs to an artist" do
+
+    NUMBER = 10
+    let!(:artist) { create :artist }
+    let!(:artist2) { create :artist }
+
+    def generate_number_of_songs(number = NUMBER)
+      number.times do
+        artist.songs.create(name: "Song Name")
+      end
+    end
+
+    it "belongs to an artist" do
+      song = artist.songs.build(name: "Song Name")
+
+      expect(song.artist).to eq(artist)
+    end
+
+    it "has one artist" do
+      song = artist.songs.build(name: "Song Name")
+      song.artist = artist2
+
+      expect(artist2.songs).not_to include(song)
+    end
+
+    it "destroys a song" do
+      generate_number_of_songs
+
+      artist.songs.first.destroy
+      expect(artist.songs.count).to match(NUMBER - 1)
+    end
   end
 end
